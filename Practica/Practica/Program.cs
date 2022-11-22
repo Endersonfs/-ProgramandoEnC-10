0dd33b45736f208217aa3343f826575dd0bb1047
Enderson Florian <eflorian@phoenixcalibrationdr.com>
2022-11-22 10:34:23 -0400

    Agregando proyecto para practicas

---
+++ Practica/Practica/Program.cs
@@ -0,0 +1,29 @@
+﻿var temperatura = 38;
+var permitirValorNull = string.Empty;
+
+//utilizando and
+var mensaje = temperatura switch
+{
+    37 => "No tienes ninguna condicion",
+    > 37 and <  39  => "Tienes fiebre",
+    >= 39 and < 43 => "Debes ir al medico",
+    < 37 => "Puede que tengas baja la azucar",
+    _ => throw new ApplicationException("Temperatura Incorrecta")
+};
+
+Console.WriteLine(mensaje);
+
+//utilizando or
+
+var dia  = DateTime.Now;
+
+var estacion = dia.Month switch
+{
+    3 or 4 or 5 => "Primavera",
+    6 or 7 or 8 => "Verano",
+    9 or 10 or 11 => "Otono",
+    12 or 1 or 2 => "Invierno",
+    _ => throw new ApplicationException("Mes Incorrecto")
+};
+
+Console.WriteLine(estacion);
--- Variable/Variable/Program.cs
+++ Variable/Variable/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+int alturaEnCentimetros = 180;
+int distanciDelaTierraAlSolEnKilometros = 150_000_000;
+int numeroSuperGrade = int.MaxValue;
+int numeroSuperPequeno = int.MinValue;
+
+Console.WriteLine(alturaEnCentimetros);
+Console.WriteLine(distanciDelaTierraAlSolEnKilometros);
+Console.WriteLine(numeroSuperGrade);
+Console.WriteLine(numeroSuperPequeno);
+
+///
 Console.WriteLine("Hello, World!");
